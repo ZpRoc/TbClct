b8d53f89752a3644d8a5cfd26e5932e121931629
ZpRoc <zheng_roc@163.com>
2021-04-07 14:41:16 +0800

    Create BaseForm.OkCancelForm.

--- TbClct/BaseForm/OkCancelForm.Designer.cs
+++ TbClct/BaseForm/OkCancelForm.Designer.cs
@@ -28,19 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonCancel.Location = new System.Drawing.Point(267, 308);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(8);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(100, 36);
+            this.buttonCancel.TabIndex = 108;
+            this.buttonCancel.Text = "取 消";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Font = new System.Drawing.Font("YaHei Consolas Hybrid", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.buttonOK.Location = new System.Drawing.Point(151, 308);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(8);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(100, 36);
+            this.buttonOK.TabIndex = 107;
+            this.buttonOK.Text = "确 定";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
             // 
             // OkCancelForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.KeyPreview = true;
             this.Name = "OkCancelForm";
             this.Text = "OkCancelForm";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OkCancelForm_KeyDown);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        public System.Windows.Forms.Button buttonCancel;
+        public System.Windows.Forms.Button buttonOK;
     }
 }
--- TbClct/BaseForm/OkCancelForm.cs
+++ TbClct/BaseForm/OkCancelForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TbClct.BaseForm
@@ -16,5 +9,74 @@ namespace TbClct.BaseForm
         {
             InitializeComponent();
         }
+
+        // ------------------------------------ Events ------------------------------------ //
+        // -------------------------------------------------------------------------------- //
+        // -------------------------------------------------------------------------------- //
+
+        /// <summary>
+        /// 确定 按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 关闭窗体并返回
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 取消 按钮事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 关闭窗体并返回
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 窗体快捷键事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OkCancelForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                // 回车按键，直接点击确定
+                if (e.KeyCode == Keys.Enter)
+                {
+                    buttonOK_Click(null, null);
+                }
+                // Esc 直接关闭窗口
+                else if (e.KeyCode == Keys.Escape)
+                {
+                    buttonCancel_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
